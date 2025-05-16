5bc866522fc56eb31c92ed0fe60718ce76e4f04e
hubirto <hubert.rybicki.hr@gmail.com>
2025-05-16 17:01:06 +0200

    Add test to department entity

--- MAS/MP5/src/main/java/pl/edu/pjwstk/mp5/repository/DepartmentRepository.java
+++ MAS/MP5/src/main/java/pl/edu/pjwstk/mp5/repository/DepartmentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 import pl.edu.pjwstk.mp5.entity.Department;
 
 @Repository
-public interface DepartmentRepository extends JpaRepository<Long, Department> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
--- MAS/MP5/src/test/java/pl/edu/pjwstk/mp5/repository/DepartmentRepositoryTest.java
+++ MAS/MP5/src/test/java/pl/edu/pjwstk/mp5/repository/DepartmentRepositoryTest.java
@@ -1,7 +1,36 @@
 package pl.edu.pjwstk.mp5.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import pl.edu.pjwstk.mp5.entity.Department;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
 class DepartmentRepositoryTest {
 
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Test
+    public void testRequiredRepositories() {
+        assertNotNull(departmentRepository);
+    }
+
+    @Test
+    public void testFetchDepartments() {
+        Iterable<Department> all = departmentRepository.findAll();
+        for (Department department : all) {
+            System.out.println(department.toString());
+        }
+    }
+
+    @Test
+    public void testSaveDepartment() {
+        Department newDept = Department.builder().name("Dept HR").location("Warsaw").budget(1_000_000).build();
+        departmentRepository.save(newDept);
+        assertEquals(1, departmentRepository.count());
+    }
 }
